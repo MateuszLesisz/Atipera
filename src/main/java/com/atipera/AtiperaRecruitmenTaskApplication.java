33022094c033ba7b97aeaaf95ac74d77f345bf70
Mateusz Lesisz <m.lesisz97@wp.pl>
2023-06-04 10:38:20 +0200

    Initial commit

---
+++ src/main/java/com/atipera/AtiperaRecruitmenTaskApplication.java
@@ -0,0 +1,13 @@
+package com.atipera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaRecruitmenTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperaRecruitmenTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atipera/AtiperaRecruitmenTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaRecruitmenTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
