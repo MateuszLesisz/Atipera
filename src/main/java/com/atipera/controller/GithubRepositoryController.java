f6e231cb763906321595dc6ca1b32f01b9c52c4c
Mateusz Lesisz <m.lesisz97@wp.pl>
2023-06-05 19:57:36 +0200

    add header exception

--- src/main/java/com/atipera/controller/GithubRepositoryController.java
+++ src/main/java/com/atipera/controller/GithubRepositoryController.java
@@ -3,10 +3,7 @@ package com.atipera.controller;
 import com.atipera.model.Github;
 import com.atipera.service.GithubService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +15,7 @@ public class GithubRepositoryController {
     private final GithubService githubService;
 
     @GetMapping("{username}")
-    public List<Github> getAllRepos(@PathVariable String username) {
-        return githubService.createGithubResponse(username);
+    public List<Github> getAllRepos(@RequestHeader(value = "Accept") String header, @PathVariable String username) {
+        return githubService.createGithubResponse(username, header);
     }
 }
